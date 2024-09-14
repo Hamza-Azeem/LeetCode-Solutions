8e0c70c7699250ed0b7e8bcb33b36dbb27ca98b1
Hamza Azeem <69521611+Hamza-Azeem@users.noreply.github.com>
2024-09-14 18:55:31 +0300

    [14] [Time Beats: 16.31%] [Memory Beats: 17.77%] - LeetPush

--- 14-Longest-Common-Prefix.java
+++ 14-Longest-Common-Prefix.java
@@ -1,22 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String result = "";
-        boolean flag = false;
-        for(int i=0;i<strs[0].length(); i++){
-            for(int j=1;j<strs.length;j++){
-                if(i >= strs[j].length()){
-                    flag = true;
-                    break;
-                }
-                if(strs[j].charAt(i) != strs[0].charAt(i)){
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag){
+        Arrays.sort(strs);
+        for(int i=0;i<Math.min(strs[0].length(), strs[strs.length - 1].length());i++){
+            if(strs[0].charAt(i) != strs[strs.length - 1].charAt(i)){
                 break;
             }
-            result+=strs[0].charAt(i);
+            result += strs[0].charAt(i);
         }
         return result;
     }
