8c168ac398aae156cc1cf52dc6c542602087033f
Hamza Azeem <69521611+Hamza-Azeem@users.noreply.github.com>
2024-09-30 11:47:59 +0300

    [15] [Time Beats: 6.04%] [Memory Beats: 25.79%] - LeetPush

--- 15-3Sum.java
+++ 15-3Sum.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i=0;i<nums.length-2;i++)
-        {
-            if(i > 0 && nums[i] == nums[i-1])
-                continue;
-            int l=i+1, r= nums.length-1;
-            while(l < r)
-            {
-                int sum = nums[i]+nums[l]+nums[r];
-                if(sum < 0)
-                {
-                    l++;
-                }else if(sum > 0)
-                {
+        Set<List<Integer>> result = new HashSet<>();
+        for(int i=0;i<nums.length - 2;i++){
+            int l = i+1, r = nums.length-1;
+            while(l < r){
+                int sum = nums[l] + nums[r] + nums[i];
+                if(sum > 0){
                     r--;
-                }
-                else
-                {
-                    List<Integer> list = new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])); 
-                    if(set.contains(list))
-                    {
-                        l++;
-                        continue;
-                    }
-                    set.add(list);
+                }else if(sum < 0){
+                    l++;
+                }else{
+                    List<Integer> list = Arrays.asList(nums[l], nums[r], nums[i]);
+                    result.add(list);
                     l++;
                 }
             }
         }
-        return new ArrayList<>(set);
+        List<List<Integer>> output = new ArrayList<>();
+        for(List<Integer> l: result){
+            output.add(l);
+        }
+        return output;
     }
 }
